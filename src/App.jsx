cafd2bee08d7a07afe6aaa62e5c054ce2c00c665
alamindev07 <alamin.dev07@gmail.com>
2026-09-20 09:38:03 +0600

    Project structure setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-4xl font-bold text-center mt-20">
+        Dev Stack
+      </h1>
+    </div>
+  );
+};
+
+export default App;
